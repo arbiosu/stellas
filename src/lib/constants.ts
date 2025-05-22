b53223c250fccfd9d55e511cd49853fb5e6ba234
Arberim Ame <amea@oregonstate.edu>
2025-05-21 20:29:48 -0400

    Initial UI

---
+++ src/lib/constants.ts
@@ -0,0 +1,66 @@
+import type { MenuItemPreview } from '@/components/menu';
+
+export const MENU_ITEMS: MenuItemPreview[] = [
+  {
+    type: 'Pizza',
+    description: 'Square or Round New York Style Thin Crust',
+    src: '/pizza.svg',
+  },
+  {
+    type: 'Wings',
+    description: 'Classic Boneless or Buffalo Wings',
+    src: '/wings.svg',
+  },
+  {
+    type: 'Appetizers',
+    description: 'Classic Italian Appetizers',
+    src: '/appetizers.svg',
+  },
+  {
+    type: 'Salads',
+    description: 'Served with garlic bread and a side of dressing',
+    src: '/salad.svg',
+  },
+  {
+    type: 'Steaks',
+    description: '1/2 lb steak served on fresh italian rolls',
+    src: '/bread.svg',
+  },
+  {
+    type: 'Burgers',
+    description: '1/2 lb Angus beef served on a brioche bun',
+    src: '/burgers.svg',
+  },
+  {
+    type: 'Pasta Dinners',
+    description: 'Penne, Spaghetti or Fettucine',
+    src: '/pasta.svg',
+  },
+  {
+    type: 'Strombolis & Calzones',
+    description: 'Classic Italian Strombolis and Calzones',
+    src: '/pizza.svg',
+  },
+
+  {
+    type: 'Wraps',
+    description: 'Large white tortilla wraps',
+    src: '/wraps.svg',
+  },
+  {
+    type: 'Paninis',
+    description: 'Served on Ciabatta bread',
+    src: '/bread.svg',
+  },
+  {
+    type: 'Classic Italian Subs',
+    description: 'Your favorite Italian subs',
+    src: '/bread.svg',
+  },
+  {
+    type: 'Hoagies',
+    description:
+      'Served with EVOO, lettuce, tomatoes, onions, oregano, salt & pepper on an Italian roll',
+    src: '/bread.svg',
+  },
+];
