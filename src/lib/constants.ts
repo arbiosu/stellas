874c088bbf567f0dffdff4b0bdf5c23fa916a92e
Arberim Ame <amea@oregonstate.edu>
2025-05-31 17:25:03 -0400

    Add menu snippets

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,62 +5,74 @@ export const MENU_ITEMS: MenuItemPreview[] = [
     type: 'Pizza',
     description: 'Square or Round New York Style Thin Crust',
     src: '/pizza.svg',
+    menuSrc: '/menu/pizza.png',
   },
   {
     type: 'Wings',
     description: 'Classic Boneless or Buffalo Wings',
     src: '/wings.svg',
+    menuSrc: '/menu/wings.png',
   },
   {
     type: 'Appetizers',
     description: 'Classic Italian Appetizers',
     src: '/appetizers.svg',
+    menuSrc: '/menu/appetizers.png',
   },
   {
     type: 'Salads',
     description: 'Served with garlic bread and a side of dressing',
     src: '/salad.svg',
+    menuSrc: '/menu/salads.png',
   },
   {
     type: 'Steaks',
     description: '1/2 lb steak served on fresh italian rolls',
     src: '/bread.svg',
+    menuSrc: '/menu/steaks.png',
   },
   {
     type: 'Burgers',
     description: '1/2 lb Angus beef served on a brioche bun',
     src: '/burgers.svg',
+    menuSrc: '/menu/burgers.png',
   },
   {
     type: 'Pasta Dinners',
     description: 'Penne, Spaghetti or Fettucine',
     src: '/pasta.svg',
+    menuSrc: '/menu/pasta.png',
   },
   {
     type: 'Strombolis & Calzones',
     description: 'Classic Italian Strombolis and Calzones',
     src: '/pizza.svg',
+    menuSrc: '/menu/strombolis.png',
   },
 
   {
     type: 'Wraps',
     description: 'Large white tortilla wraps',
     src: '/wraps.svg',
+    menuSrc: '/menu/wraps.png',
   },
   {
     type: 'Paninis',
     description: 'Served on Ciabatta bread',
     src: '/bread.svg',
+    menuSrc: '/menu/paninis.png',
   },
   {
     type: 'Classic Italian Subs',
     description: 'Your favorite Italian subs',
     src: '/bread.svg',
+    menuSrc: '/menu/subs.png',
   },
   {
     type: 'Hoagies',
     description:
       'Served with EVOO, lettuce, tomatoes, onions, oregano, salt & pepper on an Italian roll',
     src: '/bread.svg',
+    menuSrc: '/menu/hoagies.png',
   },
 ];
